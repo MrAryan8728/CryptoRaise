47c3c2703b5b9608fc4c7836d609212023d46f47
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2023-10-19 00:32:38 +0530

    footer completed

--- cryptoraise/Components/Footer/Footer.jsx
+++ cryptoraise/Components/Footer/Footer.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer Getting Rendered</div>
+    <div>
+      <hr/>
+      {/* Left side */}
+      <div className=' flex gap-3 justify-between m-3 text-lg font-bold items-baseline'>
+        <div className=' flex gap-3'>
+          <div className=' flex gap-4 border-r-2 px-3 border-black'>
+            <img className=' w-10 h-10 rounded-md bg-orange-400'/>
+            <p>CryptoRaise</p>
+          </div>
+          <div>
+            <p className=' text-green-500'>Copyright © 2023 www.etherHub.com</p>
+          </div>
+        </div>
+        {/* Right side */}
+        <div className=' flex gap-3'>
+          <img className=' w-10 h-10 rounded-md bg-orange-400'/>
+          <img className=' w-10 h-10 rounded-md bg-orange-400'/>
+          <img className=' w-10 h-10 rounded-md bg-orange-400'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
